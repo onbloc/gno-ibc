708c3d9c1d0c7af166615de3ec2a9539c8fae475
benluelo <git@luelo.dev>
2026-05-07 09:21:05 +0300

    feat: more nix stuff, connection handshake boilerplate

--- flake.nix
+++ flake.nix
@@ -9,10 +9,17 @@
       type = "github";
       owner = "gnolang";
       repo = "gno";
+      # ref = "feat/cometbls-groth16-verifier";
       rev = "e16676eec5f75ab563d4ade83e17d4a96ea04aee";
       flake = false;
     };
-
+    gnopls = {
+      type = "github";
+      owner = "gnoverse";
+      repo = "gnopls";
+      rev = "32e82ac207a551ee04fce8559b96e70daca083f9";
+      flake = false;
+    };
   };
 
   outputs =
@@ -50,22 +57,9 @@
             meta = {
               mainProgram = "gno";
             };
-            doCheck = false;
-            subPackages = [ "./gnovm/cmd/gno" ];
-            ldflags = [
-              "-X github.com/gnolang/gno/gnovm/pkg/gnoenv._GNOROOT=${inputs.gno}"
-            ];
-          };
-          gnokey = pkgs.pkgsStatic.buildGo124Module {
-            name = "gno";
-            src = inputs.gno;
-            vendorHash = "sha256-8kuyN44JcnwTM0z4IdxqMdUMb7zhghfhwMx2UAW/TBw=";
-            meta = {
-              mainProgram = "gnokey";
-            };
             env.CGO_ENABLED = 0;
             doCheck = false;
-            subPackages = [ "./gno.land/cmd/gnokey" ];
+            subPackages = [ "./gnovm/cmd/gno" "./gno.land/cmd/gnokey" ];
             ldflags = [
               "-X github.com/gnolang/gno/gnovm/pkg/gnoenv._GNOROOT=${inputs.gno}"
             ];
@@ -89,12 +83,7 @@
           };
           gnopls = pkgs.pkgsStatic.buildGo125Module rec {
             name = "gnopls";
-            src = pkgs.fetchFromGitHub {
-              owner = "gnoverse";
-              repo = "gnopls";
-              rev = "32e82ac207a551ee04fce8559b96e70daca083f9";
-              hash = "sha256-0E2Q93537xx6tVm/8oLUYgxKQ4+1NrEnTmMz8uK+xW4=";
-            };
+            src = inputs.gnopls;
             subPackages = [ "." ];
             vendorHash = "sha256-BD5lx+iTrj4GInH1gIyjj6B+DLPv3VGs5OpnvM0jFok=";
             ldflags = [
@@ -112,7 +101,6 @@
             inherit
               gno
               gnopls
-              gnokey
               gnodev
               ;
           };
@@ -121,7 +109,6 @@
               buildInputs = [
                 self'.packages.gno
                 self'.packages.gnopls
-                self'.packages.gnokey
                 self'.packages.gnodev
               ]
               ++ (with pkgs; [
@@ -131,6 +118,7 @@
                 moreutils
                 nixd
                 typescript-language-server
+                gofumpt
               ]);
             };
             nativeBuildInputs = [
@@ -158,7 +146,6 @@
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
         # those are more easily expressed in perSystem.
-
       };
     };
 }
